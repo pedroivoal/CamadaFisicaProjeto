bc83ffee6ca5bfc652bf2bec20bde28dc6f13556
pedroivoal <pedroivoal@al.insper.edu.br>
2023-02-23 14:28:49 -0300

    hi corn

--- P2 - Client-Server/aplicacaoClient.py
+++ P2 - Client-Server/aplicacaoClient.py
@@ -10,9 +10,11 @@
 #para acompanhar a execução e identificar erros, construa prints ao longo do código! 
 
 
-from enlace import *
-import time
 import numpy as np
+import time
+
+from enlace import *
+import sorteador as sort
 
 # voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
 #   para saber a sua porta, execute no terminal :
@@ -30,38 +32,44 @@ def main():
         print("Iniciou o main")
         #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
         #para declarar esse objeto é o nome da porta.
-        com1 = enlace(serialName)
-        
-
+        com1 = enlace('COM4')
+        i = 0
         # Ativa comunicacao. Inicia os threads e a comunicação seiral 
         com1.enable()
         #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
         print("Abriu a comunicação")
         
-        com1.enable()
-        time.sleep(0.2)
-        com1.sendData(b'00')
-        time.sleep(1)
+        # com1.enable()
+        # print(i);i = 1
+        # time.sleep(0.2)
+        # print(i);i = 2
+        # com1.sendData(b'00')
+        # time.sleep(1)
+        
+        txBuffer = b''  #isso é um array de bytes
         
         #aqui você deverá gerar os dados a serem transmitidos. 
         #seus dados a serem transmitidos são um array bytes a serem transmitidos. Gere esta lista com o 
         #nome de txBuffer. Esla sempre irá armazenar os dados a serem enviados.
-
-
-        #txBuffer = imagem em bytes!
-        txBuffer = b'\x12\x13\xAA'  #isso é um array de bytes
-       
+        lista_comandos = sort.sorteia()
+        for comando in lista_comandos:
+            txBuffer += bytes([len(sort.dic_comands[comando])])
+            txBuffer += sort.dic_comands[comando]
+            time.sleep(0.02)
+        txBuffer += bytes([11]) # indica o fim da transmissão
+        
+        print(txBuffer)
         print("meu array de bytes tem tamanho {}" .format(len(txBuffer)))
         #faça aqui uma conferência do tamanho do seu txBuffer, ou seja, quantos bytes serão enviados.
         
-            
+        
         #finalmente vamos transmitir os todos. Para isso usamos a funçao sendData que é um método da camada enlace.
         #faça um print para avisar que a transmissão vai começar.
         #tente entender como o método send funciona!
         #Cuidado! Apenas trasmita arrays de bytes!
         
         com1.sendData(np.asarray(txBuffer))  #as array apenas como boa pratica para casos de ter uma outra forma de dados
-          
+        
         # A camada enlace possui uma camada inferior, TX possui um método para conhecermos o status da transmissão
         # O método não deve estar fincionando quando usado como abaixo. deve estar retornando zero. Tente entender como esse método funciona e faça-o funcionar.
         txSize = com1.tx.getStatus()
--- P2 - Client-Server/sorteador.py
+++ P2 - Client-Server/sorteador.py
@@ -7,3 +7,17 @@ def sorteia():
         a = randint(1, 9)
         lista_comandos.append(a)
     return lista_comandos
+
+dic_comands = {
+                1: b'\x00\x00\x00\x00',
+                2: b'\x00\x00\xAA\x00',
+                3: b'\xAA\x00\x00',
+                4: b'\x00\xAA\x00',
+                5: b'\x00\x00\xAA',
+                6: b'\x00\xAA',
+                7: b'\xAA\x00',
+                8: b'\x00',
+                9: b'\xFF'
+               }
+
+# print(len(dic_comands[9]))
